4b631676e79bae6bae56d8122328c909ef9788c3
Kevin Newton <kddnewton@gmail.com>
2022-10-24 12:00:26 -0400

    Simplify tests

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -1,34 +1,27 @@
 import * as prettier from "prettier";
 import plugin from "../src/plugin.js";
 
-async function checkFormat(before, after) {
-  const formatted = await prettier.format(before, {
-    parser: "brainfuck",
-    plugins: [plugin]
-  });
-
-  return {
-    pass: formatted.trim() === after.trim(),
-    message: () => `Expected:\n${after}\nReceived:\n${formatted}`
-  };
-}
-
-expect.extend({
-  toChangeFormat: checkFormat,
-  toMatchFormat: (before) => checkFormat(before, before)
-});
-
 describe("plugin", () => {
-  test("does not wrap anything when it all fits", () => {
-    return expect("++++----<<<<>>>>....,,,,[[[[]]]]").toMatchFormat();
+  test("does not wrap anything when it all fits", async () => {
+    const source = "++++----<<<<>>>>....,,,,[[[[]]]]\n";
+    const formatted = await prettier.format(source, {
+      parser: "brainfuck",
+      plugins: [plugin]
+    });
+
+    expect(formatted).toEqual(source);
   });
 
-  test("wraps loops when it hits the breaks", () => {
+  test("wraps loops when it hits the breaks", async () => {
     const inner = "++++----++++----++++----++++----++++----++++";
-    const content = `++++----[${inner}${inner}${inner}${inner}]----++++`;
+    const source = `++--[${inner}${inner}${inner}${inner}]--++\n`;
 
-    return expect(content).toChangeFormat(
-      `++++----[\n  ${inner}${inner}${inner}${inner}\n]----++++`
-    );
+    const expected = `++--[\n  ${inner}${inner}${inner}${inner}\n]--++\n`;
+    const formatted = await prettier.format(source, {
+      parser: "brainfuck",
+      plugins: [plugin]
+    });
+
+    expect(formatted).toEqual(expected);
   });
 });
